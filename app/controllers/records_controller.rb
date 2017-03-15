9dcd7a34bc4801949527c22d41190bb6b4847cb3
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-15 18:50:09 +0545

    simple form to add new record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,14 @@ class RecordsController < ApplicationController
 	def index
 		@records = Record.all
 	end
+
+	def create
+		@record = Record.create(record_params)
+	end
+
+	private
+
+	def record_params
+		params.require(:record).permit(:title, :amount, :date)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
   root 'records#index'
+
+  resources :records
 end
