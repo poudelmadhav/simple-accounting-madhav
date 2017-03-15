82b88bf67868fdb51cd41d6f583a6c49e238f62d
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-15 19:10:00 +0545

    editing and updating controller added

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,6 +5,20 @@ class RecordsController < ApplicationController
 
 	def create
 		@record = Record.create(record_params)
+		redirect_to root_path
+	end
+
+	def edit
+	  @record = Record.find(params[:id])
+	end
+
+	def update
+	  @record = Record.find(params[:id])
+	  if @record.update(record_params)
+	    redirect_to root_path
+	  else
+	    redirect_to edit_record_path(params[:id])
+	  end
 	end
 
 	private
