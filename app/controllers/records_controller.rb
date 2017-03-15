9b08b079c8f5f9e4ee48029bcca3b6288f58fb41
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-15 20:55:07 +0545

    minor error fixed

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,16 +1,17 @@
 class RecordsController < ApplicationController
 	def index
-		@records = Record.all.order('date DESC').paginate(:page => params[:page], :per_page => 3)
+		@records = Record.all.order('date DESC').paginate(:page => params[:page], :per_page => 30)
 	end
 
 	def create
 		@record = Record.create(record_params)
 		if @record.valid?
 	    	flash[:success] = "A record has been successfully created!"
+	    	redirect_to root_path
 	 	else
 	    	flash[:alert] = "Woops! Looks like there has been an error!"
+	    	redirect_to new_record_path
 	 	end
-  		redirect_to root_path
 	end
 
 	def edit
@@ -31,7 +32,7 @@ class RecordsController < ApplicationController
 	def destroy
 	  @record = Record.find(params[:id])
 	  @record.destroy
-	  flash[:success] = "The idea was successfully deleted!"
+	  flash[:success] = "The record was successfully deleted!"
 	  redirect_to root_path
 	end
 
