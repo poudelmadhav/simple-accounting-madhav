2bcea887a019071b213b99fc3ae8b8d5a467a118
Madhav Paudel <poudelmadhav143@gmail.com>
2017-03-15 23:21:59 +0545

    calculation of simple accounting to model added

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,15 @@
 class Record < ApplicationRecord
 	validates :title, :amount, :date, presence: true
+
+	def self.balance
+		where("amount > ?", 0).sum("amount") 
+	end
+
+	def self.debt
+		where("amount < ?", 0).sum("amount")
+	end
+
+	def self.total
+		sum("amount")
+	end
 end
